11c03d3882321c60896c10aaa7c55f8f64dc9495
IwJhun <dlfdnd0725@gmail.com>
2018-05-25 17:25:06 +0900

    add : smart ctl 관련 기능 추가

--- bin/fabfile.py
+++ bin/fabfile.py
@@ -3,16 +3,17 @@ from fabric.api import run, env, sudo
 env.hosts =['some-user-id@127.0.0.1:10112']
 env.password = 'some-user-password'
 
+
 def pwd():
     run('pwd')
+
 
 def shutdown():
     sudo('shutdown -P 00')
 
 
-
-
-
-
+# fab smart_mon:/dev/sdb
+def smart_mon(dev='/dev/sda'):
+    sudo('smartctl --all ' + dev)
 
 
